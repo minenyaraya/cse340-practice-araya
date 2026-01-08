590ed5bc3658dfdaf813375ddde63a7406cfdae8
minenyaraya <minenyaraya@gmail.com>
2026-01-07 19:02:16 -0600

    Weeek 1

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+const name = process.env.NAME; // <-- NEW
+app.get("/", (req, res) => {
+  res.send(`Welcome, ${name}!`); // <-- UPDATED
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
