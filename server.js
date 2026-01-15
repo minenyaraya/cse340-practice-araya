35d6b9b5bcf8ef38b7ae63e9f46ecb11ac8fb648
minenyaraya <minenyaraya@gmail.com>
2026-01-14 18:01:56 -0600

    week 2
    week 2

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const name = process.env.NAME; // <-- NEW
 app.get("/", (req, res) => {
   res.send(`Welcome, ${name}!`); // <-- UPDATED
 });
-const PORT = 3000;
+const NODE_ENV = process.env.NODE_ENV || "production";
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
